0f54eee8872f8b2ca26620cbce460ccd8b0362dd
Elsa Gounot <elsa.gounot@gmail.com>
2018-03-07 12:49:41 +0100

    restaurants & reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Cô My Cantine',
+    address:      '18, rue de Menilmontant, 75020 Paris',
+    phone_number: '01 43 66 94 10',
+    category:     'japanese'
+  },
+  {
+    name:         'Bones',
+    address:      '43 rue Godefroy Cavaignac, 75011 Paris',
+    phone_number: '09 80 75 32 08',
+    category:     'french'
+  },
+  {
+    name:         "L'échappée",
+    address:      '38 rue Boyer, 75020 Paris',
+    phone_number: '01 47 97 44 58',
+    category:     'chinese'
+  },
+  {
+    name:         'Chez Sommay',
+    address:      '70 rue Rebeval, 75019 Paris',
+    phone_number: '01 42 40 83 81',
+    category:     'chinese'
+  },
+  {
+    name:         "Quedubon",
+    address:      '22 rue du Plateau, 75019 Paris',
+    phone_number: '01 42 38 18 65',
+    category:     'french'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
